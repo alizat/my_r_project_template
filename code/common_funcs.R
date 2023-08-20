35ff7932a012143492494029d2b04859fd66e2e8
Ali Ezzat <ali_ezzat85@yahoo.com>
2023-08-20 22:55:23 +0300

    add read_entire_xlsx() to common_funcs.R

---
+++ code/common_funcs.R
@@ -0,0 +1,12 @@
+read_entire_xlsx <- function(xlsx_file) {
+    all_sheets <- readxl::excel_sheets(xlsx_file)
+    all_sheets <-
+        all_sheets %>%
+        map(~ readxl::read_xlsx(xlsx_file, sheet = .x, col_types = 'text') %>% mutate_all(as.character)) %>%
+        set_names(all_sheets)
+    if (length(all_sheets) == 1) all_sheets[[1]] else all_sheets
+}
+
+is_number <- function(x) {
+    suppressWarnings({!is.na(as.numeric(x))})
+}
