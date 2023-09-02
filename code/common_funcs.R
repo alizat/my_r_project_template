776e1c5858fdbceb543f14e37f0ed6fb83fecb8d
Ali Ezzat <ali_ezzat85@yahoo.com>
2023-09-02 22:26:36 +0300

    add roxygen docs

--- code/common_funcs.R
+++ code/common_funcs.R
@@ -1,3 +1,13 @@
+#' Read all sheets of an xlsx file
+#'
+#' @description \code{read_entire_xlsx()} loads all sheets of a given xlsx file.
+#'
+#' @param xlsx_file an xlsx file
+#'
+#' @return
+#' If the specified xlsx file contains a single sheet, a data frame is returned.
+#' If there are multiple sheets then a list of data frame is returned (one data
+#' frame for each sheet).
 read_entire_xlsx <- function(xlsx_file) {
     all_sheets <- readxl::excel_sheets(xlsx_file)
     all_sheets <-
@@ -7,10 +17,23 @@ read_entire_xlsx <- function(xlsx_file) {
     if (length(all_sheets) == 1) all_sheets[[1]] else all_sheets
 }
 
+#' Is it a number?
+#'
+#' @param x the element to determine if it is a number (more precisely, whether
+#'   \code{x} can be coerced into a number)
+#'
+#' @return \code{TRUE} if \code{x} is a number, \code{FALSE} otherwise
+#'
+#' @examples
+#' is_number(4)    # TRUE
+#' is_number('4')  # TRUE
+#' is_number('A')  # FALSE
+#'
+#' is_number(c('5D', '9.5', '18', 'apple', NA, ''))
+#' # FALSE  TRUE  TRUE  FALSE  FALSE  FALSE
+#'
+#' is_number(NA_real_)        # FALSE
+#' is_number(c(FALSE, TRUE))  # TRUE  TRUE (because false can be coerced into 0)
 is_number <- function(x) {
     suppressWarnings({!is.na(as.numeric(x))})
-}
-
-equals_na <- function(a, b) {
-    if_else(a == b | (is.na(a) & is.na(b)),  TRUE, FALSE) %>% replace_na(FALSE)
 }
