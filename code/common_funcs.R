3842a5f2dbc8d3e3c127f29378d21fdb9776660c
Ali Ezzat <ali_ezzat85@yahoo.com>
2023-09-04 11:23:22 +0300

    add common fn: str_squish_consecutive()

--- code/common_funcs.R
+++ code/common_funcs.R
@@ -1,13 +1,16 @@
 #' Read all sheets of an xlsx file
 #'
-#' @description \code{read_entire_xlsx()} loads all sheets of a given xlsx file.
+#' @description
+#'   \code{read_entire_xlsx()} loads all sheets of a given xlsx file and
+#'   returns them as a list of data frames (or as a single data frame if the
+#'   xlsx file contains only a single sheet).
 #'
 #' @param xlsx_file an xlsx file
 #'
 #' @return
-#' If the specified xlsx file contains a single sheet, a data frame is returned.
-#' If there are multiple sheets then a list of data frame is returned (one data
-#' frame for each sheet).
+#'   If the specified xlsx file contains a single sheet, a data frame is returned.
+#'   If there are multiple sheets then a list of data frame is returned (one data
+#'   frame for each sheet).
 read_entire_xlsx <- function(xlsx_file) {
     all_sheets <- readxl::excel_sheets(xlsx_file)
     all_sheets <-
@@ -17,12 +20,14 @@ read_entire_xlsx <- function(xlsx_file) {
     if (length(all_sheets) == 1) all_sheets[[1]] else all_sheets
 }
 
+
+
 #' Is it a number?
 #'
 #' @param x the element to determine if it is a number (more precisely, whether
 #'   \code{x} can be coerced into a number)
 #'
-#' @return \code{TRUE} if \code{x} is a number, \code{FALSE} otherwise
+#' @return TRUE if \code{x} is a number, FALSE otherwise
 #'
 #' @examples
 #' is_number(4)    # TRUE
@@ -36,4 +41,27 @@ read_entire_xlsx <- function(xlsx_file) {
 #' is_number(c(FALSE, TRUE))  # TRUE  TRUE (because false can be coerced into 0)
 is_number <- function(x) {
     suppressWarnings({!is.na(as.numeric(x))})
+}
+
+
+
+#' Reduce identical consecutive characters in a string into a single character
+#'
+#' @description
+#'   \code{str_squish_consecutive()} reduces any consecutive identical
+#'   characters into a single character. Additionally, it removes any leading or
+#'   trailing whitespace characters.
+#'
+#' @param string a given input string
+#'
+#' @return the same input string with the consecutive identical character reduced
+#'
+#' @examples
+#' str_squish_consecutive('apple')  # "aple"
+#' str_squish_consecutive('   this isss   a    MESSY string.   ')  # "this is a MESY string."
+str_squish_consecutive <- function(string) {
+    v <- stringr::str_split_1(string, '')
+    vr <- rle(v)
+    vr$lengths[] <- 1
+    stringr::str_squish(paste(inverse.rle(vr), collapse = ''))
 }
